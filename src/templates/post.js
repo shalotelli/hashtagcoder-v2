7964ff57bf73b0b766e9f8fdfe3942547eabcc21
Sha Alibhai <shalotelli@gmail.com>
2020-03-20 08:31:22 -0400

    clean up post styling

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {graphql} from 'gatsby'
 
 import Layout from '../components/layout'
+import '../css/post.css'
 
 export const pageQuery = graphql`
   query($path: String!) {
