36952ec25a525c044392d8c5a1f1184daa442c75
Sha Alibhai <shalotelli@gmail.com>
2020-03-22 14:44:28 -0400

    change learn to courses

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const LearnPage = () => (
+const CoursesPage = () => (
   <Layout>
-    <SEO title="Learn" />
+    <SEO title="Courses" />
 
     <h1>Programmer <small><em>(noun)</em></small></h1>
-    <h2>A person who fixed a problem that you don't know you have, in a way you don't understand.</h2>
+    <h4>A person who fixed a problem that you don't know you have, in a way you don't understand.</h4>
   </Layout>
 )
 
-export default LearnPage
+export default CoursesPage
