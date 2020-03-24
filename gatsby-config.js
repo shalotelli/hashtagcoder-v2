d7b445c3c31045f823f887a1e5f001cb8647230c
Sha Alibhai <shalotelli@gmail.com>
2020-03-24 07:37:05 -0400

    add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,7 @@ module.exports = {
     },
     
     'gatsby-plugin-robots-txt',
+    'gatsby-plugin-sitemap',
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
