5fa5e6c597898271eac09f751cdd2a906c764eb6
Sha Alibhai <shalotelli@gmail.com>
2020-03-27 11:49:08 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,13 @@ module.exports = {
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-sitemap',
 
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-148371012-1'
+      }
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
