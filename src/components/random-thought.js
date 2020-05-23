7184a547d1ee8252a143acf9a9867331ff3d784c
Sha Alibhai <shalotelli@gmail.com>
2020-05-23 13:52:55 -0400

    add twitter above the fold on homepage

--- src/components/random-thought.js
+++ src/components/random-thought.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 const RandomThought = ({thought}) => (
   <div className="text-xs sm:text-base mt-4">
@@ -8,6 +10,13 @@ const RandomThought = ({thought}) => (
     </Link>
 
     <p className="text-gray-500">{thought.description}</p>
+
+    <a href="https://twitter.com/hashtagcoder" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      className="text-base sm:text-lg underline text-blue-500 hover:text-blue-800">
+      Find me on <FontAwesomeIcon icon={faTwitter} /> (@hashtagcoder) and share your thoughts with me!
+    </a>
   </div>
 )
 
