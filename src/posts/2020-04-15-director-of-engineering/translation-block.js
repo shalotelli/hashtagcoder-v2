3d03abed596dd34681b15298bd0162e237909683
Sha Alibhai <shalotelli@gmail.com>
2020-05-27 21:28:23 -0400

    clean up doe post translation block

--- src/posts/2020-04-15-director-of-engineering/translation-block.js
+++ src/posts/2020-04-15-director-of-engineering/translation-block.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TranslationBlock = () => (
-  <div class="bg-gray-100 border-l-4 border-gray-500 text-gray-800 p-4 mb-4 rounded" role="alert">
+  <div class="bg-gray-200 border-l-4 border-gray-500 text-gray-800 p-4 mb-4 rounded" role="alert">
     <h6 className="mb-2">This page has been translated!</h6>
     <div className="text-sm">
       <p>You can now read this post in different languages. Thank you so much for those that put in the time and hard work to translate these thoughts so that others can value. If you want to translate this post to your native language, please shoot me a <a href="https://twitter.com/hashtagcoder" rel="noopener noreferrer" target="_blank">message on twitter (@hashtagcoder)</a>.</p>
